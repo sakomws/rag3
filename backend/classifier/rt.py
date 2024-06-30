e65bcced39469200dee42ac3ee25338f98fbddf7
dreamflyfreya <dreamfly@sas.upenn.edu>
2024-06-29 17:48:18 -0700

    real time data

---
+++ backend/classifier/rt.py
@@ -0,0 +1,37 @@
+import openai
+import yfinance as yf
+
+def real_time_data(query,stock_name="TSLA"):
+    msft = yf.Ticker(stock_name)
+
+    # get all stock info
+
+    # get historical market data
+    hist = msft.history(period="1d", interval='1m')
+    
+    hist.to_csv('stock_data.csv', index=False)
+
+    # with open('path_to_your_file.txt', 'r') as file:
+    #   file_content = file.read()
+
+
+    # Initialize OpenAI client
+    client = openai.OpenAI(api_key="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")
+
+    # with open('stock_data.csv', 'rb') as f:
+    #     response = openai.File.create(
+    #         purpose='answers',
+    #         file=f
+    #     )
+    # file_id = response['id']
+
+    response = client.chat.completions.create(
+        model="gpt-4-1106-preview",
+        messages=[
+            {"role": "system", "content": f"You are a helpful assistant here is the stock data: {hist}, you should not include disclaimer, just the answer, even if you are not sure"},
+            {"role": "user", "content": query},
+        ],
+       # files=[file_id]
+    )
+
+    print(response.choices[0].message.content)
